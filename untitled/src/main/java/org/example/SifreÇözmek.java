277e69f2ec3b9f3a71b9730cff44e94ef0b677ab
slymnhlm123 <142054022+slymnhlm123@users.noreply.github.com>
2025-02-19 17:31:08 +0300

    Şifre Çözme classı bileşenler oluşturudu

--- untitled/src/main/java/org/example/SifreÇözmek.java
+++ untitled/src/main/java/org/example/SifreÇözmek.java
@@ -1,3 +1,33 @@
 package org.example;
-public class SifreÇözmek {
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SifreÇözmek
+{
+    JFrame frame = new JFrame("Şifreleme");
+    JTextField mesaj = new JTextField();
+    JTextField şifresayısı = new JTextField();
+    JButton gönder = new JButton("Gönder");
+    JButton geri = new JButton("Geri");
+
+    String SYMBOLS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public SifreÇözmek() {
+        frame.setSize(400, 250);
+        frame.setLayout(new GridLayout(4, 2));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        gönder.addActionListener(this);
+        geri.addActionListener(this);
+
+        frame.add(new JLabel("Mesaj:"));
+        frame.add(mesaj);
+        frame.add(new JLabel("Şifre Anahtarı (0-25):"));
+        frame.add(şifresayısı);
+        frame.add(gönder);
+        frame.add(geri);
+
+
+        frame.setVisible(true);
 }

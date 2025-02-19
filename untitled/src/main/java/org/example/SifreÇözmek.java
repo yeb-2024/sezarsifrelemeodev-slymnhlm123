1f0525ef9882e53c305fac1e94496cc1d4c7cd18
slymnhlm123 <142054022+slymnhlm123@users.noreply.github.com>
2025-02-19 17:32:48 +0300

    sifre çözme işlemi yapıldı ve action performand metodu dolduruldu

--- untitled/src/main/java/org/example/SifreÇözmek.java
+++ untitled/src/main/java/org/example/SifreÇözmek.java
@@ -2,8 +2,10 @@ package org.example;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class SifreÇözmek
+public class SifreÇözmek implements ActionListener
 {
     JFrame frame = new JFrame("Şifreleme");
     JTextField mesaj = new JTextField();
@@ -30,4 +32,47 @@ public class SifreÇözmek
 
 
         frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == gönder) {
+            String getmesaj = mesaj.getText().toUpperCase();
+            int  getkey = Integer.parseInt(şifresayısı.getText());;
+
+            try {
+                if (getkey < 0 || getkey > 25) {
+                    JOptionPane.showMessageDialog(null, "Lütfen 0-25 arasında bir anahtar giriniz!");
+                    return;
+                }
+            } catch (Exception exception) {
+                JOptionPane.showMessageDialog(null, "Geçerli bir sayı giriniz!");
+                return;
+            }
+
+            String sifrelenmismesaj = caesarEncrypt(getmesaj, getkey);
+            JOptionPane.showMessageDialog(null,"Şifrelenmiş Mesaj : "+ sifrelenmismesaj,"Bilgi",JOptionPane.INFORMATION_MESSAGE);
+
+
+        } else if (e.getSource() == geri)
+        {
+            AnaSayfa sayfa = new AnaSayfa();
+            frame.dispose();
+
+        }
+    }
+
+    public String caesarEncrypt(String message, int key) {
+        String encrypted = "";
+
+        for (char karakter : message.toCharArray()) {
+            if (SYMBOLS.indexOf(karakter) != -1) {
+                int newIndex = (SYMBOLS.indexOf(karakter) - key) % 26;
+                encrypted += SYMBOLS.charAt(newIndex);
+            } else {
+                encrypted += karakter;
+            }
+        }
+        return encrypted;
+    }
 }

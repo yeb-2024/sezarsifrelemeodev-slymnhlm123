8efb2e95fd2c135cd79ea28fe3c40cc478e2343a
slymnhlm123 <142054022+slymnhlm123@users.noreply.github.com>
2025-02-19 17:33:07 +0300

    ödev bitti

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -2,7 +2,9 @@ package org.example;
 
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        AnaSayfa ss = new AnaSayfa();
 
     }
 }

826a1465163fd2f5ba6a4335ee3fbb363ef88dc2
slymnhlm123 <142054022+slymnhlm123@users.noreply.github.com>
2025-02-19 17:29:20 +0300

    sifreleme class bileşenleri oluşturuldu

--- untitled/src/main/java/org/example/Sifrelemek.java
+++ untitled/src/main/java/org/example/Sifrelemek.java
@@ -1,4 +1,39 @@
 package org.example;
 
-public class Sifrelemek {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Sifrelemek implements ActionListener {
+    JFrame frame = new JFrame("Şifreleme");
+    JTextField mesaj = new JTextField();
+    JTextField şifresayısı = new JTextField();
+    JButton gönder = new JButton("Gönder");
+    JButton geri = new JButton("Geri");
+
+    String SYMBOLS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    public Sifrelemek() {
+        frame.setSize(400, 250);
+        frame.setLayout(new GridLayout(4, 2));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        gönder.addActionListener(this);
+        geri.addActionListener(this);
+
+        frame.add(new JLabel("Mesaj:"));
+        frame.add(mesaj);
+        frame.add(new JLabel("Şifre Anahtarı (0-25):"));
+        frame.add(şifresayısı);
+        frame.add(gönder);
+        frame.add(geri);
+
+
+        frame.setVisible(true);
+    }
+
+
+
+
 }
